37d58676b10787f5e4dbca5d51deb756587b808e
David Manning <dlmanning@me.com>
2013-11-05 17:47:14 -0800

    basic server files

---
+++ server.js
@@ -0,0 +1,24 @@
+var http = require('http');
+var url  = require('url');
+var fs   = require('fs');
+
+var portNumber = 7777;
+
+http.createServer(function (request, response) {
+  var resourceName = "";
+
+  if (request.method === 'GET') {
+    resourceName = url.parse(request.url).path;
+    fs.readFile(__dirname + '/public_html' + resourceName, function (err, data) {
+      if (err) {
+        response.writeHead(404);
+        response.end("File not found");
+      } else {
+        response.writeHead(200);
+        response.end(data);
+      }
+    });
+  }
+}).listen(portNumber);
+
+console.log("Listening on port " + portNumber);
