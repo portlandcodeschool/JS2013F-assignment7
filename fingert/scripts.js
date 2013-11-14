5dfb1dfb1b31aad80c6cebfbe6aa15de2e76583a
JFingert <joshuafingert@gmail.com>
2013-11-14 13:03:02 -0800

    solved second commit

--- fingert/scripts.js
+++ fingert/scripts.js
@@ -9,24 +9,33 @@ $(function () {
   $postsbox.html(postsView(posts));
 
 
-  $('.post h2').css('color', 'red');
+   $('.post h2').css('color','red').click(function (e) {
+    $(this).nextAll().slideToggle('slow');
+  });
   $('.post h2 + p').css('font-size', '2em');
 
-//trying to display all post titles in aside bar
-  $aside = $('.title-list');
-  $aside.html(postsTitle(posts));
+
+  posts.forEach(function (item) {
+    $('.aside ul').append("<li>" + item.title + "</li>");
+    $('#post-box').append(postTemplate(item));
+  });
 
 
-    // posts.forEach(function (item) {
-    //   $('.aside ul').append("<li>" + item.title + "</li>");
-    //   $('#post-box').append(postTemplate(item));
+
+     $('.post p').not('.signature').css('font-size', '1.2em');
+    // $('.aside li').click(function (e) {
+    // $('.post h2:contains(\'' + this.innerText + '\')').nextAll().slideToggle();
     // });
 
-});
 
-
-
-      
+//   $('.aside ul li').on('click', function(event) {
+//   console.log("YO!");
+//   $('.post').each(function(thisElement){
+//     if (event.target.innerText  === $(thisElement.title).text()) {
+//       $(thisElement).parent().remove();
+//     }
+//   });
+// });
 
 
 
@@ -39,68 +48,59 @@ function postsView (posts) {
   return $(view);
 }
 
+
+var isAsideHidden = false;
+$('#page-toggler').click(function() {
+  $('.aside').slideToggle("slow", function () {
+    if (isAsideHidden === false) {
+      isAsideHidden = true;
+      $('#post-box').width("100%");
+    } else {
+      isAsideHidden = false;
+      $('#post-box').width("73%");
+    }
+
+  });
+});
+
+
+$('#sort-title').click(function () {
+    $('.post').remove();
+
+    posts.sort(function (a, b) {
+      if(a.title < b.title) return -1;
+      if(a.title > b.title) return 1;
+      return 0;
+   });
+
+    posts.forEach(function (item) {
+      $('#post-box').append(postTemplate(item));
+    });
+
+  });
+
+  $('#sort-author').click(function () {
+  $('.post').remove();
+
+  posts.sort(function (a, b) {
+    if(a.author < b.author) return -1;
+    if(a.author > b.author) return 1;
+    return 0;
+  });
+
+  posts.forEach(function (item) {
+    $('#post-box').append(postTemplate(item));
+  });
+
+  });
+
+});
+
+
 function postTemplate (post) {
   return "<div class=\"post\"><h2>" + post.title + "</h2>\n" +
   "<p>" + post.text + "</p>\n" +
   "<p class=\"signature\">" + post.author + "</p></div>";
 }
 
-//trying to display posts titles in aside bar
-function postTitle (post) {
-  return "<ul class=\"title-list\">" + post.title + "</ul>\n";
-}
-
-//removes corresponding post 
-$('.title-list li').on('click', function(event) {
-
-  $('h2').each(function(thisElement){
-    if (event.target.innerText  === $(this).text()) {
-      $(this).parent().remove();
-    }
-  });
-
-});
-
-
-//trying to order the titles for button event, not currently being called
-function titleOrder (post) {
-  // var titleArray = [];
-  // posts.forEach(function (title){
-  //   titleArray.push(posts.title);
-  //   var sortedTitles = titleArray.sort();
-  //   });
-  // return sortedTitles;
-
-  return post.sort();
-}
-
-
-
-//this button won't toggle the post-box back to normal size
-     $('.btn-info').click(function() {
-      $('.aside').slideToggle("slow", function () {
-        $('#post-box').width("100%");
-        //   function (){
-        //   $('#post-box').width("73%");
-        // });
-        // });
-        });
-      });
-
-//this is the post-alphabetizing button
-     // $('btn-inverse').click(function(){
-     //    titleOrder ();
-     //    }
-
-     
-/*
-<script>
-var modWidth = 50;
-$( "div" ).one( "click", function() {
-  $( this ).width( modWidth ).addClass( "mod" );
-  modWidth -= 8;
-});
-</script> */
-
-
-// beefy ./javascripts/scripts.js:js/main.js --live
+}); //end ready!
